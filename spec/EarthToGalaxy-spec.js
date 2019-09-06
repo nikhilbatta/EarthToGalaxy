7cb977f1e61ff9c9e83c532011e859cf17f86b23
nikhil batta <nbatta08gmail.com>
2019-09-06 10:12:50 -0700

    tries to run test for my if statement in jupiter

--- spec/EarthToGalaxy-spec.js
+++ spec/EarthToGalaxy-spec.js
@@ -1,7 +1,10 @@
 import { Human } from './../src/main.js';
+
 describe("human.jupiterAge()",function(){
   it("will grab the age and expectancy for planet jupiter", function(){
     let person = new Human(8,80)
-    expect(person.jupiterAge()).toEqual(1,160)
+    if(this.age < 12){
+      expect(person.jupiterAge()).toEqual("You're not even 1 year old on this planet!",160)
+    }
   })
 })
--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ export class Human {
     this.expectancy = expectancy;
   }
   jupiterAge(){
+    if(this.age < 12){
+      let ageForJupiter = ("You're not even 1 year old on this planet!");
+      return ageForJupiter;
+    }
+    else{
     let ageForJupiter = Math.round(this.age / 11.86)
     let expectancyForJupiter = this.expectancy * 2
     return ageForJupiter;
+    return expectancyForJupiter
+  }
   }
 }
