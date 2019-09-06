7955464d7d1eed8efa2a901120ec81d2e105d0fd
nikhil batta <nbatta08gmail.com>
2019-09-06 13:41:59 -0700

    ran all tests, now putting in if else statements for expectancy

--- spec/EarthToGalaxy-spec.js
+++ spec/EarthToGalaxy-spec.js
@@ -1,10 +1,40 @@
 import { Human1 } from './../src/earth.js';
-
 // beforeEach(function(){
 //   const person = new Human1(8,80)
 // })
-describe("Human", function(){
+describe("Human1", function(){
   it("will grab the age and expectancy for planet jupiter", function(){
-    expect(person.jupiterAge()).toEqual(33,160);
+    let person = new Human1(8,80);
+    expect(person.jupiterAge()).toEqual(1 ,160);
   });
 });
+describe("Human1", function(){
+  it("blah", function(){
+    let person = new Human1(8,80);
+    expect(person.venusAge()).toEqual( 5,60);
+  });
+});
+describe("Human1", function(){
+  it("blah2", function(){
+    let person = new Human1(8,80);
+    expect(person.marsAge()).toEqual(5,68);
+  });
+});
+describe("Human1", function(){
+  it("blah3", function(){
+    let person = new Human1(8,80);
+    expect(person.mercuryAge()).toEqual(1 ,160);
+  });
+});
+// describe("Human1", function(){
+//   it("will grab the age and expectancy for planet jupiter", function(){
+//     let person = new Human1(8,80);
+//     expect(person.jupiterAge()).toEqual(1 ,160);
+//   });
+// });
+// describe("Human1", function(){
+//   it("will grab the age and expectancy for planet jupiter", function(){
+//     let person = new Human1(8,80);
+//     expect(person.jupiterAge()).toEqual(1 ,160);
+//   });
+// });
--- src/earth.js
+++ src/earth.js
@@ -4,9 +4,39 @@ export class Human1 {
     this.expectancy = expectancy;
   }
   jupiterAge(){
-    let ageForJupiter = Math.round(this.age / 11.86);
-    let expectancyForJupiter = this.expectancy * 2;
-    return ageForJupiter;
-    return expectancyForJupiter;
+    if (this.age < 12){
+      let ageForJupiter = "You're not even 1 years old on this planet"
+      let expectancyForJupiter = 0;
+      return ageForJupiter;
+      return expectancyForJupiter;
+    } else {
+      let ageForJupiter = Math.round(this.age / 11.86);
+      let expectancyForJupiter = 74;
+      if(this.age > expectancyForJupiter){
+        expectancyForJupiter = this.age - expectancyForJupiter
+        return ageForJupiter;
+        return expectancyForJupiter;
+      }
+    }
+  }
+  venusAge(){
+    let ageForVenus = Math.round(this.age / .62);
+    let expectancyForVenus = this.expectancy * 5
+    return ageForVenus;
+    return expectancyForVenus;
+  }
+  marsAge(){
+    let ageForMars = Math.round(this.age / 1.88);
+    let expectancyForMars = this.expectancy * .1
+    return ageForMars;
+    return expectancyForMars;
+  }
+  mercuryAge(){
+    let ageForMercury = Math.round(this.age / .24);
+    let expectancyForMercury = this.expectancy * .2
+    return ageForMercury;
+    return expectancyForMercury;
   }
 }
+var person = new Human1(8,80)
+person.jupiterAge()
