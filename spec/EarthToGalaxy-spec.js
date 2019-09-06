75817deec75aef78de79a8de80f24d8e033eec31
nikhil batta <nbatta08gmail.com>
2019-09-06 11:41:55 -0700

    tried resetting hard head to go back to my working test

--- spec/EarthToGalaxy-spec.js
+++ spec/EarthToGalaxy-spec.js
@@ -1,8 +1,8 @@
 import { Human1 } from './../src/earth.js';
 
-beforeEach(function(){
-  const person = new Human1(8,80)
-})
+// beforeEach(function(){
+//   const person = new Human1(8,80)
+// })
 describe("Human", function(){
   it("will grab the age and expectancy for planet jupiter", function(){
     expect(person.jupiterAge()).toEqual(33,160);
